100b18efc6ce11ff3292a3ecebf6076cf5bf66fd
ashish <ashish.chaurasia@medibuddy.in>
2025-09-21 18:04:08 +0530

    .js file added and new code

---
+++ Scripts/credentials.js
@@ -0,0 +1,29 @@
+console.log("Credentials file loaded");
+
+output.credentials = {
+    retailMobile: '1000027633',
+    corpLogin: '1000087878',
+    mobileOTP: '9562',
+    corpEmail: 'test5@medibuddy.in',
+    emailOTP: '456123',
+    username: 'test1@wellsfargo',
+    password: '01011995'
+
+}
+output.xeroDayFirstEmailVerify = {
+    zeroDayEmail: 'test7364322@accenture.com',
+    zeroDayMobile: '1000087433',
+    zeroDayName: 'Zero day test',
+    zeroDayEmpCode: 'MB01010012'
+}
+output.xeroDayFirstMobileVerify = {
+    zeroDayEmail: 'test7364322@accenture.com',
+    zeroDayMobile: '1000087787',
+    zeroDayName: 'Zero day test',
+    zeroDayEmpCode: 'MB01010012'
+}
+output.mobile_verify = {
+    username: 'test2@hdfcergo',
+    password: '01011999',
+}
+;
